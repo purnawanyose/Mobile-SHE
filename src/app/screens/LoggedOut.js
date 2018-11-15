a9f611dba160c4384d2f7b713887e228ee8eed25
Yose purnawan <purnawan.yose@gmail.com>
2018-11-15 10:08:51 +0700

    initial project

--- src/app/screens/LoggedOut.js
+++ src/app/screens/LoggedOut.js
@@ -1,90 +1,92 @@
-import React, {
-    Component
-} from 'react';
+import React, { Component } from "react";
 import {
-    StyleSheet,
-    View,
-    Image,
-    Text,
-    StatusBar,
-    TouchableHighlight
-} from 'react-native';
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  StatusBar,
+  TouchableHighlight
+} from "react-native";
 import {
-    Container,
-    Header,
-    Title,
-    Content,
-    Footer,
-    FooterTab,
-    Button,
-    Left,
-    Right,
-    Body
-} from 'native-base'; 
-import Icon from 'react-native-vector-icons/FontAwesome';
-import LinearGradient from 'react-native-linear-gradient';
+  Container,
+  Header,
+  Title,
+  Content,
+  Footer,
+  FooterTab,
+  Button,
+  Left,
+  Right,
+  Body
+} from "native-base";
+import Icon from "react-native-vector-icons/FontAwesome";
+import LinearGradient from "react-native-linear-gradient";
 
-import styles from './styles/LoggedOut';
-import colors from '../../styles/colors'; 
-import RoundedButton from '../components/RoundedButton';
+import styles from "./styles/LoggedOut";
+import colors from "../../styles/colors";
+import RoundedButton from "../components/RoundedButton";
 
-const shelogo = require('../../assets/images/shelogo.png'); 
+const shelogo = require("../../assets/images/shelogo.png");
+const silogo = require("../../assets/images/Logo-Semen-Indonesia.png");
+const grlogo = require("../../assets/images/Logo-Semen-Gresik.png");
+const pdlogo = require("../../assets/images/Logo-Semen-Padang.png");
+const tnlogo = require("../../assets/images/Logo-Semen-Tonasa.png");
 
 export default class LoggedOut extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    render() {
-        return ( 
-            
-            <Container style={styles.wrapper}>
-            
-                 <StatusBar
-                    backgroundColor={colors.green03}
-                    barStyle="light-content"
-                    />
-                <LinearGradient style={styles.welcomeWrapper} colors={['#227093','#00838F','#7ed6df']}>
-                <Image
-                        source={shelogo}
-                        style={styles.logo}
-                    />
-                    <Text style={styles.welcomeText}> 
-                     Welcome to SHE Mobile. </Text>
-                    <RoundedButton
-                    text="Continue with Login"
-                    textColor={colors.white}
-                    background={colors.green01}
-                    icon={<Icon name="user-circle" size={20} style={styles.facebookButtonIcon} />}
-                    handleOnPress={this.onFacebookPress}
-                    />
-                    <View style={styles.termsAndConditions}>
-                        <Text style={styles.termsText}> 
-                        By tapping continue, I agree to Safety Health Environment
-                        </Text>
-                        <Text style={styles.termsText}> 
-                        {"Mobile's "}
-                        </Text>
-                        <TouchableHighlight style={styles.linkButton}>
-                        <Text style={styles.termsText}>Terms of Services</Text>
-                        </TouchableHighlight>
-                        <Text style={styles.termsText}>, </Text>
-                        <TouchableHighlight style={styles.linkButton}>
-                        <Text style={styles.termsText}>Payments Terms of Services</Text>
-                        </TouchableHighlight>
-                        <Text style={styles.termsText}>, </Text>
-                        <TouchableHighlight style={styles.linkButton}>
-                        <Text style={styles.termsText}>and Privacy Policy </Text>
-                        </TouchableHighlight>
-                        <Text style={styles.termsText}>by Semen Indonesia. </Text>
-                    </View>
-                </LinearGradient>
-                <Footer style={styles.footer}>
-                    <Title>Footer</Title>
-                </Footer>
-            </Container>
-            
-        );
-    }
+  render() {
+    return (
+      <Container style={styles.wrapper}>
+        <StatusBar backgroundColor={colors.green03} barStyle="light-content" />
+        <LinearGradient
+          style={styles.welcomeWrapper}
+          colors={[colors.green03, colors.blue02, colors.whiteBlue]}
+        >
+          <Image source={shelogo} style={styles.logo} />
+          <Text style={styles.welcomeText}>Welcome to SHE Mobile. </Text>
+          <RoundedButton
+            text="Continue with Login"
+            textColor={colors.white}
+            background={colors.green01}
+            icon={
+              <Icon
+                name="user-circle"
+                size={20}
+                style={styles.facebookButtonIcon}
+              />
+            }
+            handleOnPress={this.onFacebookPress}
+          />
+          <View style={styles.termsAndConditions}>
+            <Text style={styles.termsText}>
+              By tapping continue, I agree to Safety Health Environment
+            </Text>
+            <Text style={styles.termsText}>{"Mobile's "}</Text>
+            <TouchableHighlight style={styles.linkButton}>
+              <Text style={styles.termsText}>Terms of Services</Text>
+            </TouchableHighlight>
+            <Text style={styles.termsText}>, </Text>
+            <TouchableHighlight style={styles.linkButton}>
+              <Text style={styles.termsText}>Payments Terms of Services</Text>
+            </TouchableHighlight>
+            <Text style={styles.termsText}>, </Text>
+            <TouchableHighlight style={styles.linkButton}>
+              <Text style={styles.termsText}>and Privacy Policy </Text>
+            </TouchableHighlight>
+            <Text style={styles.termsText}>by Semen Indonesia. </Text>
+          </View>
+        </LinearGradient>
+        <Footer style={styles.footer}>
+          <Image source={silogo} style={styles.footerLogoSI} />
+          <Image source={grlogo} style={styles.footerLogo} />
+          <Image source={pdlogo} style={styles.footerLogo} />
+          <Image source={tnlogo} style={styles.footerLogo} />
+        </Footer>
+      </Container>
+    );
+  }
 }
--- src/app/screens/styles/LoggedOut.js
+++ src/app/screens/styles/LoggedOut.js
@@ -1,58 +1,75 @@
-import { StyleSheet } from 'react-native';
-import iPhoneSize from '../../../helpers/utils';
-import colors from '../../../styles/colors'; 
+import { StyleSheet } from "react-native";
+import iPhoneSize from "../../../helpers/utils";
+import colors from "../../../styles/colors";
+import { Row } from "native-base";
 
-let termsTextSize = 13;
-let headingTextSize = 30;
-if (iPhoneSize() === 'small') {
-  termsTextSize = 12;
-  headingTextSize = 26;
+let termsTextSize = 12;
+let headingTextSize = 28;
+if (iPhoneSize() === "small") {
+  termsTextSize = 10;
+  headingTextSize = 24;
 }
 
 const styles = StyleSheet.create({
-    wrapper: {
-      flex: 1,
-      display: 'flex',
-    },
-    logo: {
-        width: 60,
-        height: 70, 
-        marginTop: 10,
-        marginBottom: 40,
-      },
-    welcomeWrapper: {
-        flex: 1,
-        display: 'flex',
-        padding: 25,
-        paddingTop: 50
-      },
-    welcomeText: {
-        fontSize: headingTextSize,
-        color: colors.white,
-        fontWeight: '300',
-        marginBottom: 40,
-      },
-    termsAndConditions: {
-        flexWrap: 'wrap',
-        alignItems: 'flex-start',
-        flexDirection: 'row',
-        marginTop: 30,
-    },
-    termsText: {
-      color: colors.white,
-      fontSize: termsTextSize,
-      fontWeight: 'normal',
-    },
-    linkButton: {
-      borderBottomWidth: 1,
-      borderBottomColor: colors.white,
-    },
-    facebookButtonIcon: {
-        color: colors.white,
-        position: 'relative',
-        left: 20,
-        zIndex: 8,
-      },
+  wrapper: {
+    flex: 1,
+    display: "flex"
+  },
+  logo: {
+    width: 60,
+    height: 70,
+    marginTop: 10,
+    marginBottom: 40
+  },
+  welcomeWrapper: {
+    flex: 1,
+    display: "flex",
+    padding: 25,
+    paddingTop: 50
+  },
+  welcomeText: {
+    fontFamily: "Montserrat-Medium",
+    fontSize: headingTextSize,
+    color: colors.white,
+    fontWeight: "300",
+    marginBottom: 40
+  },
+  termsAndConditions: {
+    flexWrap: "wrap",
+    alignItems: "flex-start",
+    flexDirection: "row",
+    marginTop: 30
+  },
+  termsText: {
+    fontFamily: "Montserrat-Light",
+    color: colors.white,
+    fontSize: termsTextSize,
+    fontWeight: "normal"
+  },
+  linkButton: {
+    borderBottomWidth: 1,
+    borderBottomColor: colors.white
+  },
+  facebookButtonIcon: {
+    color: colors.white,
+    position: "relative",
+    left: 20,
+    zIndex: 8
+  },
+  footer: {
+    flexDirection: "row",
+    backgroundColor: colors.whiteBlue
+  },
+  footerLogoSI: {
+    width: 60,
+    height: 40,
+    marginTop: 5
+  },
+  footerLogo: {
+    width: 40,
+    height: 40,
+    marginTop: 5
+  }
 });
-  
+
 export default styles;
